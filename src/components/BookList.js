e27460e49fe67dcb976048e7dae659163addaca5
scott <scott-lithgow@outlook.com>
2022-05-25 14:09:25 +0100

    updated class copmonents context Consumers to functional components using the use effect hook

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    render() {
-        return (
-            <ThemeContext.Consumer>{(context) => {    
-                const{ isLightTheme, light, dark } = context;
-                const theme = isLightTheme ? light : dark;
-                return(
-                    <div className = 'book-list' style = {{ color: theme.syntax, background: theme.bg }}>
+const BookList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div className = 'book-list' style = {{ color: theme.syntax, background: theme.bg }}>
                         <ul>
                             <li style = {{ background: theme.ui }}>The Way of Kings</li>
                             <li style = {{ background: theme.ui }}>The Name of the Wind</li>
                             <li style = {{ background: theme.ui }}>The Final Empire</li>
                         </ul>
                     </div>
-                )}}
-            </ThemeContext.Consumer>
-        )
-    }
+     );
 }
-
+ 
 export default BookList;
